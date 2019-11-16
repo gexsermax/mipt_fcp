fc2902cce1cbca8759429304745cc62c4cd23f9a
Sergey Puchinin <gexsermax@yandex.ru>
2019-11-16 19:39:09 +0300

    Updated parsing client options

--- client/include/client.h
+++ client/include/client.h
@@ -9,4 +9,11 @@ struct program_flags {
 };
 typedef struct program_flags flags_t;
 
+#define FILENAME_MAX_LENGTH 64
+
+/* wrong usage error message */
+#define wuprintf()                                                             \
+  fprintf(stderr, "Usage: %s [-v] -f filename \n", argv[0]);                 \
+  exit(EXIT_FAILURE);
+
 #endif /* _FCP_CLIENT_CLIENT_H */
--- client/src/client.c
+++ client/src/client.c
@@ -6,30 +6,45 @@
 /* for exit, EXIT_SUCCESS, EXIT_FAILURE */
 #include <stdlib.h>
 
+/* for getopt */
 #include <getopt.h>
+
+/* for strcpy */
+#include <string.h>
 
 int main(int argc, char *argv[], char *envp[]) {
   flags_t flags = {
     false  /* verbose */
   }; /* program flags */
+  char filename[FILENAME_MAX_LENGTH] = {};
 
-  /* Parse arguments */ {
+  /* Parse options */ {
+    bool topt = false; /* flag for t option */
     int retval = 0; /* returned value */
-    while ((retval = getopt(argc, argv, "v")) != -1) {
+    while ((retval = getopt(argc, argv, "vf:")) != -1) {
       switch (retval) {
         case 'v':
           flags.v = true;
           break;
-        default: /* '?' */
-          fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
-          exit(EXIT_FAILURE);
+        case 'f':
+          strcpy(filename, optarg);
+          topt = true;
           break;
+        default: /* '?' */
+          wuprintf();
       }
+    }
+    if (!topt) {
+      wuprintf();
     }
   }
 
   if (flags.v) {
     printf("Hello, World! I'm the Client!\n");
+  }
+
+  if (flags.v) {
+    printf("Goodbye, World! Best wishes, the Client!\n");
   }
 
   exit(EXIT_SUCCESS);
